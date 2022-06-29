502d56ed60c97d3e02a4d920540c17dd858f57b6
Caio Romanato Ruiz - Cadis - Tecnologia <caio.ruiz@cadis.com.br>
2022-06-29 10:01:05 -0300

    Sistema Hotel

---
+++ Hotel/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========================");
+            Console.WriteLine("BEM VINDO AO HOTEL MANGO");
+            Console.WriteLine("========================");
+            Console.WriteLine();
+
+            Hospede[] vetor = new Hospede[10];
+
+            Console.Write("Quantos quartos serão alugados ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Hospede(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+            }
+        }
+    }
+}
